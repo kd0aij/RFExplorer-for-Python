257ed33a5f5bdfd3a9491b5c47e8bf8aa71cdf2f
Mark Whitehorn <kd0aij@gmail.com>
2017-08-27 14:11:23 -0600

    needed for pip install

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='RFExplorer',
+      version='0.1',
+      description='python library',
+      url='http://github.com/kd0aij/RFExplorer',
+      author='Flying Circus',
+      author_email='kd0aij@gmail.com',
+      license='gplv3',
+      packages=['RFExplorer'],
+      zip_safe=False)
